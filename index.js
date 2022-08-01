aca26111fcbf1fec87ab7da9c8e2e11aeb46c2b4
Mahmoud <whixat@gmail.com>
2022-08-01 18:52:35 +0200

    getting bot's token
    The function has been more user-friendly and getting bot's token feature.

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const { default: axios } = require("axios");
-const config = require('./config.json')
+const config = require("./config.json");
 
-async function getData() {
-    const req = await axios({
-        url: "https://discord.com/api/v9/applications",
-        method: "POST",
-        headers: {
-            Authorization: config.token
-        },
-        "content-type": "application/json",
-        data: {
-            name: 'fnrtest' // bot name you want to create
-        },
-    })
-    const data = req.data
-    console.log(data)
-    const createBot = await axios({
-        url: `https://discord.com/api/v9/applications/${data.id}/bot`,
-        method: "POST",
-        headers: {
-            Authorization: config.token
-        },
-        "content-type": "applicaction/json",
-    })
-    const botData = createBot.data;
-    console.log(botData)
+async function generateBot(token, name) {
+  const req = await axios({
+    url: "https://discord.com/api/applications",
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    "content-type": "application/json",
+    data: {
+      name,
+    },
+  }).catch(console.error);
+    
+  const data = req.data;
+  console.log(data);
+    
+  await axios({
+    url: `https://discord.com/api/applications/${data.id}/bot`,
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    "content-type": "applicaction/json",
+  }).catch(console.error);
+
+  const getToken = await axios({
+    url: `https://discord.com/api/applications/${data.id}/bot/reset`,
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    "content-type": "applicaction/json",
+  }).catch(console.error);
+
+  console.log(getToken.data);
 }
 
-getData()
+generateBot(config.token, "botname"); // first parameter (account's token), second parameter (bot's name)
